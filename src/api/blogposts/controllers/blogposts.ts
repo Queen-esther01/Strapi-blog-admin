383b52f6b1a26119032d5780a94e2d11ceadf352
Esther <ejidikeesther@gmail.com>
2023-02-09 04:36:50 +0100

    Push Admin To Github

---
+++ src/api/blogposts/controllers/blogposts.ts
@@ -0,0 +1,7 @@
+/**
+ * blogposts controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::blogposts.blogposts');
---
+++ src/api/blogposts/services/blogposts.ts
@@ -0,0 +1,7 @@
+/**
+ * blogposts service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::blogposts.blogposts');
